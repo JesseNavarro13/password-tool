177d63f0d639be394ce158d2b7619c821f9486d1
jessenavarro13 <jessnav13@gmail.com>
2025-09-16 14:48:20 -0700

    Initial commit: frontend setup, backend setup, /hello endpoint

---
+++ backend/src/test/java/com/passwordtool/backend/PasswordToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passwordtool.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordToolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
